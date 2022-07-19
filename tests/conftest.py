b463f8dd6e358e13b0925012d387de5b330b4ba8
Tony Sprocket <tonysprocket@gmail.com>
2022-07-19 12:47:29 -0600

    tests pass through diff_and_sign

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,26 +7,27 @@ from starkware.starknet.testing.starknet import Starknet
 # contract and library paths
 FP_CONTRACT = os.path.join(os.path.dirname(__file__), "../contracts/fp.cairo")
 
-DECIMALS = 1 * 10**18
+DECIMALS = 10**18
+HALF_DECIMALS = 10**9
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", autouse=True)
 def event_loop():
     return asyncio.new_event_loop()
 
 
 # contract and object factories
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", autouse=True)
 async def starknet_factory():
     starknet = await Starknet.empty()
     return starknet
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", autouse=True)
 async def fp_factory(starknet_factory):
     starknet = starknet_factory
 
-    # Deploy the account contract
+    # Deploy the fixed_point contract
     fp_contract = await starknet.deploy(source=FP_CONTRACT)
 
     return fp_contract
--- tests/test_fixed_point_contract.py
+++ tests/test_fixed_point_contract.py
@@ -1,6 +1,6 @@
 import pytest
 from hypothesis import given, strategies as st, settings
-from .conftest import DECIMALS
+from .conftest import DECIMALS, HALF_DECIMALS
 
 # from OZ utils.py
 def to_uint(a):
@@ -19,14 +19,12 @@ async def test_floor_intermediate(fp_factory):
 
     a = to_uint(2345152673865427896541)
 
-    root = await fixed_point.floor_intermediate(a).call()
-    assert from_uint(root.result[0]) == (2345)
+    res = await fixed_point.floor_intermediate(a).call()
+    assert from_uint(res.result[0]) == (2345)
 
 
 @given(
-    x=st.integers(
-        min_value=1000000000000000000, max_value=1000000000000000000000000000000000000
-    ),
+    x=st.integers(min_value=10**17, max_value=10**42),
 )
 @settings(deadline=None)
 @pytest.mark.asyncio
@@ -35,8 +33,98 @@ async def test_hyp_floor_intermediate(fp_factory, x):
 
     a = to_uint(x)
 
-    root = await fixed_point.floor_intermediate(a).call()
-    assert from_uint(root.result[0]) == (x % DECIMALS)
+    res = await fixed_point.floor_intermediate(a).call()
+    assert from_uint(res.result[0]) == (x // DECIMALS)
+
+
+@pytest.mark.asyncio
+async def test_floor(fp_factory):
+    fixed_point = fp_factory
+
+    a = to_uint(2345152673865427896541)
+
+    res = await fixed_point.floor(a).call()
+    assert from_uint(res.result[0]) == (2345 * DECIMALS)
+
+
+@given(
+    x=st.integers(min_value=10**17, max_value=10**42),
+)
+@settings(deadline=None)
+@pytest.mark.asyncio
+async def test_hyp_floor(fp_factory, x):
+    fixed_point = fp_factory
+
+    a = to_uint(x)
+
+    res = await fixed_point.floor(a).call()
+    assert from_uint(res.result[0]) == ((x // DECIMALS) * DECIMALS)
+
+
+@pytest.mark.asyncio
+async def test_half_scaling(fp_factory):
+    fixed_point = fp_factory
+
+    a = to_uint(2345152673865427896541)
+
+    res = await fixed_point.half_scaling(a).call()
+    assert from_uint(res.result[0]) == (2345152673865)
+
+
+@pytest.mark.asyncio
+async def test_double_scaling(fp_factory):
+    fixed_point = fp_factory
+
+    a = to_uint(2345152673865427896541)
+
+    res = await fixed_point.double_scaling(a).call()
+    assert from_uint(res.result[0]) == (2345152673865427896541 * 10**18)
+
+
+@pytest.mark.asyncio
+async def test_add(fp_factory):
+    fixed_point = fp_factory
+
+    a = to_uint(2345152673865427896541)
+    b = to_uint(768102938476645378273)
+
+    res = await fixed_point.add(a, b).call()
+    assert from_uint(res.result[0]) == (2345152673865427896541 + 768102938476645378273)
+
+
+@pytest.mark.asyncio
+async def test_sub(fp_factory):
+    fixed_point = fp_factory
+
+    a = to_uint(2345152673865427896541)
+    b = to_uint(768102938476645378273)
+
+    res = await fixed_point.sub(a, b).call()
+    assert from_uint(res.result[0]) == (2345152673865427896541 - 768102938476645378273)
+
+
+@pytest.mark.asyncio
+async def test_diff_and_sign_false(fp_factory):
+    fixed_point = fp_factory
+
+    a = to_uint(2345152673865427896541)
+    b = to_uint(768102938476645378273)
+
+    res = await fixed_point.diff_and_sign(a, b).call()
+    assert from_uint(res.result[0]) == (2345152673865427896541 - 768102938476645378273)
+    assert res.result[1] == 0
+
+
+@pytest.mark.asyncio
+async def test_diff_and_sign_true(fp_factory):
+    fixed_point = fp_factory
+
+    a = to_uint(2345152673865427896541)
+    b = to_uint(768102938476645378273)
+
+    res = await fixed_point.diff_and_sign(b, a).call()
+    assert from_uint(res.result[0]) == (2345152673865427896541 - 768102938476645378273)
+    assert res.result[1] == 1
 
 
 '''
